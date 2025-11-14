d78eae51e76cfd2a07fdedc7f94c031be1840965
abilliansyah <abilliansyah6@gmail.com>
2025-11-14 17:47:24 +0700

    login page

--- travel-api/public/index.php
+++ travel-api/public/index.php
@@ -79,7 +79,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
