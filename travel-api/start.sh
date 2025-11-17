f08677e67d88d63ab611ac616b25bf509f5f675e
abilliansyah <abilliansyah6@gmail.com>
2025-11-17 10:29:18 +0700

    fix dockerfile11

--- travel-api/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Wajib: Jalankan PHP-FPM di background
-php-fpm -D
-
-# Wajib: Jalankan Caddy di foreground (agar container tetap hidup)
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
