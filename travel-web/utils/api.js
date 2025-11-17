e467a63d5fbb055a8e38c0f0f8be085518f0a07f
abilliansyah <abilliansyah6@gmail.com>
2025-11-17 14:53:46 +0700

    api.js

--- travel-web/utils/api.js
+++ travel-web/utils/api.js
@@ -1,6 +1,6 @@
 // utils/api.js atau di component
 async function getHello() {
-  const res = await fetch('https://my-laravel-backend.up.railway.app/api/hello');
+  const res = await fetch('https://projecttripgo-production.up.railway.app/api/hello');
   const data = await res.json(); // ubah response jadi JSON
   console.log(data.message); // Output: "Hello from Laravel"
 }
