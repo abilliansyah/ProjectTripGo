24502a5c62a82d783a4ace14a5efea2900bfcf2d
abilliansyah <abilliansyah6@gmail.com>
2025-12-19 00:27:25 +0700

    tripgo gogogo3 fix bug3

--- travel-web/utils/axiosClient.ts
+++ travel-web/utils/axiosClient.ts
@@ -1,16 +1,12 @@
-// utils/axiosClient.ts
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-
 const axiosClient = axios.create({
-  baseURL: '/',
-  // PENTING: Mengaktifkan pengiriman cookies (credentials)
-  withCredentials: true, 
+  // Gunakan env variable, jika tidak ada baru pakai localhost (untuk dev)
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000",
   headers: {
-    'Accept': 'application/json',
     'Content-Type': 'application/json',
   },
+  withCredentials: true, // Penting jika Anda menggunakan Sanctum/Cookies
 });
 
 export default axiosClient;
