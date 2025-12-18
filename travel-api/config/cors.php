b7863fdd99155afb24b20d1396e4e6bcab30bc02
abilliansyah <abilliansyah6@gmail.com>
2025-12-19 00:32:40 +0700

    tripgo gogogo3 fix bug cors

--- travel-api/config/cors.php
+++ travel-api/config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('ALLOWED_ORIGINS', '*')],
+    'allowed_origins' => ['https://project-trip-go.vercel.app','http://localhost:3000',],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
