18aa215c8570c756caff693083cdf7ca809c3b53
abilliansyah <abilliansyah6@gmail.com>
2025-12-19 00:38:32 +0700

    tripgo gogogo3 fix bug cors2

--- travel-api/config/cors.php
+++ travel-api/config/cors.php
@@ -1,10 +1,15 @@
 <?php
+
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://project-trip-go.vercel.app','http://localhost:3000',],
+    'allowed_origins' => [
+        'https://project-trip-go.vercel.app', 
+        'http://localhost:3000'
+    ],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => true, // WAJIB TRUE karena Axios pakai withCredentials
 ];
